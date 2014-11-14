52ce0e2c69761f142bf091f00e8c60663a421f11
Tao Lin <lintao51@gmail.com>
2014-11-14 17:51:14 +0800

    initialize the cicular buffer.

--- ElecDataStruct/test/test_CircularBuffer.cc
+++ ElecDataStruct/test/test_CircularBuffer.cc
@@ -5,4 +5,8 @@ int main() {
     typedef ElecData::CircularBuffer<int, 1000> CircularBufferInt;
     CircularBufferInt mybuffer;
     std::cout << CircularBufferInt::get_buffer_size() << std::endl;
+
+    typedef ElecData::CircularBuffer<float, 2000> CircularBufferFloat;
+    CircularBufferFloat mybuffer_float;
+    std::cout << CircularBufferFloat::get_buffer_size() << std::endl;
 }
